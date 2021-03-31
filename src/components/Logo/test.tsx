7455827914054e94721fc0c7c1cf633562143673
Marcelo8173 <marceloandrebio@gmail.com>
2021-03-30 22:22:49 -0300

    tdd create large ou normal render logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -14,4 +14,37 @@ describe('<Logo />', () => {
       color: '#FAFAFA'
     })
   })
+
+  it('it should the render black label by default', () => {
+    //renderizar o componente `render`
+    //selecionar o elemento a ser testado `screen (queries)`
+    //expect - assertion - asserção - comparação
+    renderWithTheme(<Logo color="black" />)
+    //metodo screen faz querie que pega a label text won games - .parentElement seleciona o elemento pai
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
+  })
+
+  it('it should be render a big logo', () => {
+    //renderizar o componente `render`
+    //selecionar o elemento a ser testado `screen (queries)`
+    //expect - assertion - asserção - comparação
+    renderWithTheme(<Logo size="large" />)
+    //metodo screen faz querie que pega a label text won games - .parentElement seleciona o elemento pai
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('it should be render a normal logo', () => {
+    //renderizar o componente `render`
+    //selecionar o elemento a ser testado `screen (queries)`
+    //expect - assertion - asserção - comparação
+    renderWithTheme(<Logo />)
+    //metodo screen faz querie que pega a label text won games - .parentElement seleciona o elemento pai
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
 })
