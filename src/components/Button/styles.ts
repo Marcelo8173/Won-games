749a9fef784b5216aae2f4e0ab07f1b3781b6482
Marcelo8173 <marceloandrebio@gmail.com>
2021-04-07 10:08:49 -0300

    feat: create button style

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,34 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+import { buttonProps } from '.'
+
+type wrapperProps = Pick<buttonProps, 'size'>
+
+const wrapperButtonModifiers = {
+  small: (theme: DefaultTheme) => css`
+    /* height: 3rem; */
+    font-size: ${theme.font.sizes.xsmall};
+    padding: ${theme.spacings.xsmall} ${theme.spacings.small};
+  `,
+  medium: (theme: DefaultTheme) => css`
+    height: 4rem;
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.xsmall} ${theme.spacings.medium};
+  `,
+  large: (theme: DefaultTheme) => css`
+    height: 5rem;
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xsmall} ${theme.spacings.large};
+  `
+}
+
+export const Wrapper = styled.button<wrapperProps>`
+  ${({ theme, size }) => css`
+    background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
+    color: ${theme.colors.white};
+    border: 0;
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xsmall};
+
+    ${!!size && wrapperButtonModifiers[size](theme)};
+  `}
+`
